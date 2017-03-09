50ba22dd0db4d9d547adb81f3cfdea2aca788a58
MiniMarker <cfmarker@gmail.com>
2017-03-09 16:01:21 +0100

    div

--- script.js
+++
@@ -1,8 +0,0 @@
-function scrollToAnchor(aid){
-    var aTag = $("a[name='"+ aid +"']");
-    $('html,body').animate({scrollTop: aTag.offset().top},'slow');
-}
-
-$("#om").click(function() {
-   scrollToAnchor('about');
-});
