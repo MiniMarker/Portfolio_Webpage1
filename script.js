a81e3670deafeec4b5808b8fae278e572ad8b89e
MiniMarker <cfmarker@gmail.com>
2017-03-09 13:27:52 +0100

    div

---
+++ script.js
@@ -0,0 +1,18 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script type="text/javascript">
+
+ $("document").ready(function(){
+
+    function scrollToAnchor(aid){
+    var aTag = $("a[name='"+ aid +"']");
+        $('html,body').animate({scrollTop: aTag.offset().top},'slow');
+    }
+
+    $("#link").click(function() {
+        scrollToAnchor('id3');
+    });
+
+
+});
+
+ </script>
